d6a26a843809c0bca0d43da68091ea54a9f776fc
leo_something <leonardomazzorana@protonmail.com>
2025-05-01 19:06:08 +0200

    fix audio pulses

--- src/AudioPulsesEffect.cpp
+++ src/AudioPulsesEffect.cpp
@@ -6,13 +6,15 @@ AudioPulsesEffect::AudioPulsesEffect(const LibGuitarMap& guitarMap) {
 
 void AudioPulsesEffect::draw() {
     for (int i = 0; i < WIDTH; i++) {
-        lines[i] = random(0, HEIGHT/2);
+        lines[i] = random(0, 2*HEIGHT/3);
     }
+    guitarMap.setRGBColor(255, 255, 255);
+    guitarMap.fill(false);
     for (int i = 0; i < WIDTH; i++) {
         int line_heigth = lines[i];
+        guitarMap.setHSVColor(random(0, 255), 255, 255);
         for(int j=0; j<line_heigth; j++){
-            guitarMap.setHSVColor(random(0, 255), 255, 255);
-            guitarMap.point(i, HEIGHT-j, false);
+            guitarMap.point(i, HEIGHT-j-1, false);
         }
     }
     guitarMap.update();
